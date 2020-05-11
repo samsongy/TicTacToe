bc5a051d677ffc0deab2c6e228d48255166a3ce3
Sam Songy <60078148+samsongy@users.noreply.github.com>
2020-05-11 15:18:48 -0500

    Added while loop to continue playing the game until user decides to stop

--- program.java
+++ program.java
@@ -1,8 +1,45 @@
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("\nWelcome to TicTacToe\n\n");   
+        
+        boolean continueGame = true;
 
-        System.out.println("This is the develop branch");
+        while(continueGame){
+
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Would you like to play again? (y/n):");
+            String userResponse = sc.nextLine();
+
+            switch(userResponse){
+                case "y":
+                    continueGame = true;
+                    break;
+                case "n":
+                    continueGame = false;
+                    break;
+                default:
+                    while(!userResponse.equals("y") && !userResponse.equals("n")){
+                        System.out.println("Please select either y or n");
+                        userResponse = sc.nextLine();
+
+                        if(userResponse.equals("y")){
+                            continueGame = true;
+                        }
+
+                        if(userResponse.equals("n")){
+                            continueGame = false;
+                        }
+                    }
+
+            }
+
+            
+
+        }
+
     }
-    
 }
