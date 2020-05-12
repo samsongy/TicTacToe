88fc3499d3d161e1c3f4ba060b9eea2c363fd6ba
Sam Songy <60078148+samsongy@users.noreply.github.com>
2020-05-12 13:08:42 -0500

    Created UpdateBoard method that takes in a marker and position on the board and fills that position with the marker

--- program.java
+++ program.java
@@ -6,6 +6,14 @@ public class Program {
     public static void Gameboard(String[][] slots){
 
         for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                if(slots[i][j] == null) {
+                    slots[i][j] = " ";
+                }
+            }
+        }
+
+        for(int i=0; i<3; i++){
             System.out.println("-------");
             for(int j=0; j<3; j++){
                 System.out.print("|" + slots[i][j]);
@@ -17,20 +25,41 @@ public class Program {
 
     }
 
+    public static void UpdateGameboard(String[][] slots, String marker, int row, int column){
+
+        slots[row][column] = marker;
+
+    }
+
     public static void main(String[] args) {
         System.out.println("\nWelcome to TicTacToe\n\n");   
 
+        Scanner sc = new Scanner(System.in);
         String[][] boardSlots = new String[3][3];
 
-        for(int i=0; i<3; i++){
-            for(int j=0; j<3; j++){
-                if(boardSlots[i][j] == null) {
-                    boardSlots[i][j] = " ";
-                }
-            }
-        }
-
         Gameboard(boardSlots);
+
+        System.out.println("\n\n");
+        
+        // System.out.print("Which marker do you want to be? (x/o): ");
+        // String marker = sc.next();
+
+        //testing
+        String marker = "x";
+        int row = 1;
+        int column = 1;
+
+
+        UpdateGameboard(boardSlots, marker, row, column);
+        Gameboard(boardSlots);
+
+
+
+
+
+
+        
+        //UpdateGameboard(boardSlots);
 
         
         //boolean continueGame = true;
